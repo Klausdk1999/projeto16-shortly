9e422343b53c1327161dba80a1b71817b57e2030
Klaus Dieter Kupper <klausdk@pop-os.localdomain>
2022-08-04 22:41:39 -0300

    feat: user/me

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -6,7 +6,7 @@ import {nanoid} from "nanoid";
 export async function postURL(req, res) {
     const { url } = req.body;
     const id  = res.locals.id;
-    console.log("id locals: " + id);
+  
     const shortUrl = nanoid(8);
 
     try {
@@ -78,23 +78,23 @@ export async function deleteURLById(req, res) {
   const url_id  = req.params.id;
 
   try {
-    console.log("url_id "+url_id)
+  
     //verificar se url é do usuario
     const user_id = res.locals.id;
-    console.log("user_id "+user_id)
+    
     const selectResponse = await connection.query(
       `SELECT * FROM urls WHERE urls.id=$1;`
       , [url_id]
     );
-    console.log("selectresponse.rows "+ selectResponse.rows[0])
+
     if(selectResponse.rows.length<1){
       return res.sendStatus(404);
     }
-    
+
     let deleteResponse;
 
     if(selectResponse.rows[0].user_id==user_id){
-      console.log("if delete passou")
+     
       //deletar
       deleteResponse = await connection.query(
         `DELETE FROM urls WHERE urls.id=$1;`
@@ -105,8 +105,6 @@ export async function deleteURLById(req, res) {
       return res.sendStatus(401);
     }
 
-   console.log("deleteresponse:"+ deleteResponse)
-
     if(deleteResponse.rowCount<1){
       return res.sendStatus(404);
     }
@@ -116,5 +114,96 @@ export async function deleteURLById(req, res) {
   } catch (error) {
     return res.status(500).send(error);
   }
+} 
+
+export async function getUser(req, res) {
+
+  try {
+    
+    //verificar se url é do usuario
+    const user_id = res.locals.id;
+    
+    const shortenedUrls = await connection.query(
+      `SELECT * FROM urls WHERE urls.user_id=$1;`
+      , [user_id]
+    );
+
+    if(shortenedUrls.rows.length<1){
+      return res.sendStatus(404);
+    }
+    
+    let visitCount=0;
+
+    for(let i=0;i<shortenedUrls.rows.length;i++){
+      visitCount += shortenedUrls.rows[i].visit_count;
+    }
+
+    const selectedUser = await connection.query(
+      `SELECT * FROM users WHERE users.id=$1;`
+      , [user_id]
+    );
+
+    if(selectedUser.rows.length<1){
+      return res.sendStatus(404);
+    }
+    
+    const rows=shortenedUrls.rows;
+
+    const responseObject={
+      "id": selectedUser.rows[0].id,
+	    "name": selectedUser.rows[0].name,
+	    "visitCount": visitCount,
+	    "shortenedUrls": rows
+    }
+
+    return res.status(200).send(responseObject);
+
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+} 
+
+export async function getRanking(req, res) {
+//   const url_id  = req.params.id;
+
+//   try {
+//     console.log("url_id "+url_id)
+//     //verificar se url é do usuario
+//     const user_id = res.locals.id;
+//     console.log("user_id "+user_id)
+//     const selectResponse = await connection.query(
+//       `SELECT * FROM urls WHERE urls.id=$1;`
+//       , [user_id]
+//     );
+//     console.log("selectresponse.rows "+ selectResponse.rows[0])
+//     if(selectResponse.rows.length<1){
+//       return res.sendStatus(404);
+//     }
+
+//     let deleteResponse;
+
+//     if(selectResponse.rows[0].user_id==user_id){
+//       console.log("if delete passou")
+//       //deletar
+//       deleteResponse = await connection.query(
+//         `DELETE FROM urls WHERE urls.id=$1;`
+//         , [url_id]
+//       );
+
+//     }else{
+//       return res.sendStatus(401);
+//     }
+
+//    console.log("deleteresponse:"+ deleteResponse)
+
+//     if(deleteResponse.rowCount<1){
+//       return res.sendStatus(404);
+//     }
+
+//     return res.sendStatus(200);
+
+//   } catch (error) {
+//     return res.status(500).send(error);
+//   }
 } 
 
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteURLById, postURL,getURLById,getShortURL } from "../controllers/urlsController.js";
+import { getRanking, getUser , deleteURLById , postURL,getURLById,getShortURL } from "../controllers/urlsController.js";
 import { tokenValidation } from "../middlewares/tokenSchemaValidation.js";
 import { validateURL } from "../middlewares/urlSchemaValidation.js";
 
@@ -9,4 +9,7 @@ router.post("/urls/shorten", tokenValidation, validateURL ,postURL);
 router.get("/urls/:id", getURLById);
 router.get("/urls/open/:shortUrl", getShortURL);
 router.delete("/urls/:id",tokenValidation, deleteURLById);
+router.get("/users/me", tokenValidation, getUser);
+router.get("/ranking", getRanking);
+
 export default router;
